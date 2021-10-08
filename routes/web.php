496ec8c365bd6bb4a0741e0aa1c52f1cd5e250a1
Raditya Gilang Wicaksono <rgilang30@gmail.com>
2021-10-08 10:12:37 +0700

    Tugas 5. Menyelesaikan video ke 4 tentang Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -32,13 +32,15 @@ Route::get('/about', function () {
 Route::get('/blog', function () {
     $blog_posts = [
         [
-            "title" => "Judul Pertama",
-            "aouthor" => "Raditya",
+            "tittle" => "Judul Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Raditya",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum vero adipisci libero necessitatibus, eligendi cupiditate dolore suscipit at? Iste eaque fugit distinctio est tempore minus, placeat, modi omnis blanditiis totam dolor, incidunt expedita. Corrupti, debitis? Dolores ipsam repellendus assumenda quis ratione, voluptatem necessitatibus delectus fuga dolorem similique aut. Dicta provident similique, ut pariatur deserunt quasi nesciunt maxime modi magni neque enim quas nulla perspiciatis quam numquam autem quod cupiditate quae distinctio, et iste dolorum nihil ex tempore. Ab, tempore. Reiciendis."
         ],
         [
-            "title" => "Judul Kedua",
-            "aouthor" => "Gilang Wicaksono",
+            "tittle" => "Judul Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Gilang Wicaksono",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa reiciendis fuga. Quis molestias libero neque dolore veritatis voluptatum placeat dolores aliquam incidunt deleniti molestiae nihil, accusantium ex illo cupiditate assumenda labore totam delectus veniam eveniet excepturi atque aperiam voluptatibus? Neque, veniam laudantium nisi maxime accusamus odio cupiditate assumenda obcaecati temporibus eius expedita quae beatae esse et quo aut quisquam minima similique sequi aliquam optio aperiam asperiores vero facilis? Voluptate ipsam id voluptates unde vitae laudantium facere optio molestias laboriosam quibusdam fuga reprehenderit voluptatibus dolorum ullam, dolores repellendus quia. Odit?"
         ]
     ];
@@ -48,4 +50,34 @@ Route::get('/blog', function () {
         "posts" => $blog_posts
     ]);
     // return 'Halaman Blog';
+});
+
+//halaman single post
+Route::get('posts/{slug}', function($slug){
+    $blog_posts = [
+        [
+            "tittle" => "Judul Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Raditya",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum vero adipisci libero necessitatibus, eligendi cupiditate dolore suscipit at? Iste eaque fugit distinctio est tempore minus, placeat, modi omnis blanditiis totam dolor, incidunt expedita. Corrupti, debitis? Dolores ipsam repellendus assumenda quis ratione, voluptatem necessitatibus delectus fuga dolorem similique aut. Dicta provident similique, ut pariatur deserunt quasi nesciunt maxime modi magni neque enim quas nulla perspiciatis quam numquam autem quod cupiditate quae distinctio, et iste dolorum nihil ex tempore. Ab, tempore. Reiciendis."
+        ],
+        [
+            "tittle" => "Judul Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Gilang Wicaksono",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa reiciendis fuga. Quis molestias libero neque dolore veritatis voluptatum placeat dolores aliquam incidunt deleniti molestiae nihil, accusantium ex illo cupiditate assumenda labore totam delectus veniam eveniet excepturi atque aperiam voluptatibus? Neque, veniam laudantium nisi maxime accusamus odio cupiditate assumenda obcaecati temporibus eius expedita quae beatae esse et quo aut quisquam minima similique sequi aliquam optio aperiam asperiores vero facilis? Voluptate ipsam id voluptates unde vitae laudantium facere optio molestias laboriosam quibusdam fuga reprehenderit voluptatibus dolorum ullam, dolores repellendus quia. Odit?"
+        ]
+    ];
+    
+    $new_post = [];
+    foreach($blog_posts as $post){
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "tittle" => "Single Post",
+        "post" => $new_post
+    ]);
 });
