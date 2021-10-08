8077071d86611c6e0df9997c4168c30bb47b91bb
Raditya Gilang Wicaksono <rgilang30@gmail.com>
2021-10-08 09:17:15 +0700

    Tugas 5. Menyelesaikan video ke 1 sampai ke 3, dan video ke 4 belum selesai

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,39 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>WPU Blog | Home</title>
+</head>
+<body>
+    <h1>Halaman Home</h1>
+
+    <script src="js/script.js"></script>
+</body>
+</html> --}}
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+
+<title>RGW Blog | {{ $tittle }}</title>
+</head>
+<body>
+    
+    @include('partials.navbar')
+
+    <div class="container mt-4">
+        @yield('container')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "tittle" => "Home"
+    ]);
+    // return 'Halaman Home';
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "tittle" => "About",
+        "name" => "Raditya Gilang Wicaksono",
+        "image" => "doodoosonic-0t7PNNZlTC0-unsplash.jpg"
+    ]);
+    // return 'Halaman About';
+});
+
+Route::get('/blog', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Pertama",
+            "aouthor" => "Raditya",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum vero adipisci libero necessitatibus, eligendi cupiditate dolore suscipit at? Iste eaque fugit distinctio est tempore minus, placeat, modi omnis blanditiis totam dolor, incidunt expedita. Corrupti, debitis? Dolores ipsam repellendus assumenda quis ratione, voluptatem necessitatibus delectus fuga dolorem similique aut. Dicta provident similique, ut pariatur deserunt quasi nesciunt maxime modi magni neque enim quas nulla perspiciatis quam numquam autem quod cupiditate quae distinctio, et iste dolorum nihil ex tempore. Ab, tempore. Reiciendis."
+        ],
+        [
+            "title" => "Judul Kedua",
+            "aouthor" => "Gilang Wicaksono",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa reiciendis fuga. Quis molestias libero neque dolore veritatis voluptatum placeat dolores aliquam incidunt deleniti molestiae nihil, accusantium ex illo cupiditate assumenda labore totam delectus veniam eveniet excepturi atque aperiam voluptatibus? Neque, veniam laudantium nisi maxime accusamus odio cupiditate assumenda obcaecati temporibus eius expedita quae beatae esse et quo aut quisquam minima similique sequi aliquam optio aperiam asperiores vero facilis? Voluptate ipsam id voluptates unde vitae laudantium facere optio molestias laboriosam quibusdam fuga reprehenderit voluptatibus dolorum ullam, dolores repellendus quia. Odit?"
+        ]
+    ];
+    
+    return view('posts', [
+        "tittle" => "Posts",
+        "posts" => $blog_posts
+    ]);
+    // return 'Halaman Blog';
+});
