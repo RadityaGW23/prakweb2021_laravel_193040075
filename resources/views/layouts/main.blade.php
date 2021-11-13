12204130406b58857be5bb4dd635b1758fa13579
Raditya Gilang Wicaksono <rgilang30@gmail.com>
2021-11-13 20:45:17 +0700

    Tugas 8. Menyelesaikan video ke 14 View Login & Registration

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -23,6 +23,12 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    
+    {{-- Bootstrap Icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+
+    {{-- Style CSS --}}
+    <link rel="stylesheet" href="/css/style.css">
 
 <title>RGW Blog | {{ $title }}</title>
 </head>
